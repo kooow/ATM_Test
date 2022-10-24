aae71cc1430ec33ea327381818718cc4dc5d9524
RobertKovacs <kooow@windowslive.com>
2022-10-24 21:43:56 +0200

    added WithdrawalController with validation

---
+++ ATM_Test/Controllers/WithdrawalController.cs
@@ -0,0 +1,51 @@
+﻿using ATM_Test.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATM_Test.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    [Produces("application/json")]
+    public class WithdrawalController : ControllerBase
+    {
+        private readonly ILogger<WithdrawalController> _logger;
+
+        private readonly IDepositService _depositService;
+
+        public WithdrawalController(ILogger<WithdrawalController> logger, IDepositService depositService)
+        {
+            _logger = logger;
+            _depositService = depositService;
+        }
+
+        private static bool IsValid(ulong sum)
+        {
+            if ( (sum % 1000) == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        [HttpPost("withdrawal")]
+        [ProducesResponseType(typeof(Dictionary<string, uint>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        public ActionResult Withdrawal(ulong sum)
+        {
+            bool valid = IsValid(sum);
+            if (!valid)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+
+            return Ok(new Dictionary<string, uint>());
+        }
+    }
+}
