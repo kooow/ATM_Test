d2cf1aa84287e1994847862ba3d7f155f17281cc
RobertKovacs <kooow@windowslive.com>
2022-10-24 16:30:17 +0200

    added Denomation and Enumeration classes

---
+++ ATM_Test/Models/Denomination.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ATM_Test.Entities
+{
+    public class Denomation : Enumeration
+    {
+        public static readonly Denomation Thousand = new(1000);
+        public static readonly Denomation TwoThousand = new(2000);
+        public static readonly Denomation FiveThousand = new(5000);
+        public static readonly Denomation TenThousand = new(10000);
+        public static readonly Denomation TwentyThousand = new(20000);
+
+        public Denomation(uint key) : base(key) { }
+    }
+
+}
---
+++ ATM_Test/Models/Enumeration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+
+namespace ATM_Test.Entities
+{
+    public abstract class Enumeration : IComparable
+    {
+        public uint Key { get; private set; }
+
+        protected Enumeration(uint key) => (Key) = (key);
+
+        public override string ToString() => Key.ToString();
+
+        public static IEnumerable<T> GetAll<T>() where T : Enumeration =>
+            typeof(T).GetFields(BindingFlags.Public |
+                                BindingFlags.Static |
+                                BindingFlags.DeclaredOnly)
+                     .Select(f => f.GetValue(null))
+                     .Cast<T>();
+
+        public override bool Equals(object obj)
+        {
+            if (obj is not Enumeration otherValue)
+            {
+                return false;
+            }
+
+            var typeMatches = GetType().Equals(obj.GetType());
+            var valueMatches = Key.Equals(otherValue.Key);
+
+            return typeMatches && valueMatches;
+        }
+
+        public int CompareTo(object other) => Key.CompareTo(((Enumeration)other).Key);
+
+    }
+
+}
